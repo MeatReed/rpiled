5580ab778f1ad42a0a178a8f4e5b02f44a5177c8
MeatReed <tim.oumpho@gmail.com>
2023-10-09 23:39:23 +0200

    fix bug

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80, host="0.0.0.0")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
